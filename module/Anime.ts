22754be70209d48143a52acd35b9a4f3491383a1
Tiffceet <zkang.loo@gmail.com>
2021-03-09 23:56:55 +0800

    Stop showing author tag for certain queries

--- module/Anime.ts
+++ module/Anime.ts
@@ -292,7 +292,7 @@ export class Anime implements ModuleInterface {
 					book_embeds.push(this.nhentai_read_embed(book, k + 1, true));
 				}
 
-				Util.paginated(origin, book_embeds, max_page, page_num, `Page {n} of {max} | @${origin.author.tag}`, ["⏮️", "⬅️", "➡️", "⏭️"], 300000);
+				Util.paginated(origin, book_embeds, max_page, page_num, `Page {n} of {max}`, ["⏮️", "⬅️", "➡️", "⏭️"], 300000);
 				break;
 			case "search":
 				if (args.length == 1) {
@@ -318,7 +318,7 @@ export class Anime implements ModuleInterface {
 					search_result_embeds.push(this.nhentai_info_embed(result.books[k], "", true));
 				}
 
-				Util.paginated(origin, search_result_embeds, result.books.length, 1, `Result {n} of {max} | @${origin.author.tag}`, ["⏮️", "⬅️", "➡️", "⏭️"], 300000);
+				Util.paginated(origin, search_result_embeds, result.books.length, 1, `Result {n} of {max}`, ["⏮️", "⬅️", "➡️", "⏭️"], 300000);
 
 				break;
 		}
