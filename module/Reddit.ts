81d458e078b3649ec9ffeca6c164d6e582f1712a
Tiffceet <zkang.loo@gmail.com>
2021-03-10 21:47:11 +0800

    Added RedditEmbedSettings - RedditEmbedSettings that indicates how a specific reddit post should be sent back to the user

--- module/Reddit.ts
+++ module/Reddit.ts
@@ -1,13 +1,14 @@
 import * as Discord from "discord.js";
 import { ModuleInterface } from "./ModuleInterface";
+import { RedditEmbedSettings } from "../interface/module/Reddit/RedditEmbedSettings";
 const fetch = require("node-fetch");
 export class Reddit implements ModuleInterface {
-    constructor() {
-        this._init_status = true;
-    }
-    _init_status: boolean = false;
-    _worker (origin: Discord.Message, cmd_name: string, args: string[]) {
-        if (origin == null) {
+	constructor() {
+		this._init_status = true;
+	}
+	_init_status: boolean = false;
+	_worker(origin: Discord.Message, cmd_name: string, args: string[]) {
+		if (origin == null) {
 			return;
 		}
 		let reddit_name = this.command_reddit[cmd_name];
@@ -15,13 +16,13 @@ export class Reddit implements ModuleInterface {
 			return;
 		}
 		this.send_reddit_post(origin, reddit_name);
-    }
+	}
 
-    command_reddit: Record<string, string> = {
-		tellajoke: "ShortCleanFunny",
-		scareme: "TwoSentenceHorror",
-		cursedfood: "cursedfoods",
-		food: "Food",
+	command_reddit: Record<string, RedditEmbedSettings> = {
+		tellajoke: { reddit_name: "ShortCleanFunny", include_image: false },
+		scareme: { reddit_name: "TwoSentenceHorror", include_image: false },
+		cursedfood: { reddit_name: "cursedfoods", include_image: true },
+		food: { reddit_name: "Food", include_image: true }
 	}
 
 	// =============================================================
@@ -30,15 +31,13 @@ export class Reddit implements ModuleInterface {
 	/**
 	 *  Send a random reddit post from a reddit page !
 	 * @param {Discord.Message} origin origin of the command sender
-	 * @param {string} reddit_name reddit to fetch post from
-	 * @param {boolean} include_image (optional) will send image if true
+	 * @param {RedditEmbedSettings} settings 
 	 */
-	async send_reddit_post (
-		origin:Discord.Message,
-		reddit_name:string,
-		include_image = false
+	async send_reddit_post(
+		origin: Discord.Message,
+		settings: RedditEmbedSettings
 	) {
-		let url = `https://www.reddit.com/r/${reddit_name}/.json?sort=hot&t=week&limit=300`;
+		let url = `https://www.reddit.com/r/${settings.reddit_name}/.json?sort=hot&t=week&limit=300`;
 		let HTMLResponse = await fetch(url);
 		let json_response = await HTMLResponse.json();
 
@@ -51,16 +50,16 @@ export class Reddit implements ModuleInterface {
 				.setTitle(json_response.data.children[len].data.title)
 				.setDescription(json_response.data.children[len].data.selftext)
 				.setColor("#" + ((Math.random() * 0xffffff) << 0).toString(16));
-            
-            if(include_image) {
-                reddit_post.setImage(json_response.data.children[len].data.url);
-            }
+
+			if (settings.include_image) {
+				reddit_post.setImage(json_response.data.children[len].data.url);
+			}
 
 			origin.channel.send(reddit_post);
 		} catch (err) {
 			console.log(err);
 			origin.channel.send(
-				`Error getting post from r/${reddit_name}, contact Looz !`
+				`Error getting post from r/${settings.reddit_name}, contact Looz !`
 			);
 		}
 	}
