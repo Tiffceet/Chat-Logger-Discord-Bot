a89ecbd629e4560b1af1a17a9e4441f896304d6c
Tiffceet <zkang.loo@gmail.com>
2021-12-20 20:55:34 +0800

    Update activity.ts
    Update discord activity application ID

--- src/commands/activity.ts
+++ src/commands/activity.ts
@@ -1,84 +1,107 @@
 import Command from '../interface/Command'
-import {SlashCommandBuilder} from '@discordjs/builders'
+import { SlashCommandBuilder } from '@discordjs/builders'
 import fetch from 'node-fetch'
-const activity:Command = {
+const activity: Command = {
 	data: new SlashCommandBuilder()
 		.setName('activity')
 		.setDescription('Initiate a discord activity')
-		.addStringOption(opt=>opt
-			.setName('activity_type')
-			.setRequired(true)
-			.setDescription('Activity to start')
-			.addChoices([
-				['youtube_together', 'youtube_together'],
-				['watch_together_dev', 'watch_together_dev'],
-				['fishington', 'fishington'],
-				['chess_in_the_park', 'chess_in_the_park'],
-				['chess_in_the_park_dev', 'chess_in_the_park_dev'],
-				['betrayal', 'betrayal'],
-				['doodlecrew', 'doodlecrew'],
-				['wordsnacks', 'wordsnacks'],
-				['lettertile', 'lettertile'],
-				['poker_night', 'poker_night'],
-			])),
+		.addStringOption((opt) =>
+			opt
+				.setName('activity_type')
+				.setRequired(true)
+				.setDescription('Activity to start')
+				.addChoices([
+					['youtube', 'youtube'],
+					['youtubedev', 'youtubedev'],
+					['poker', 'poker'],
+					['betrayal', 'betrayal'],
+					['fishing', 'fishing'],
+					['chess', 'chess'],
+					['chessdev', 'chessdev'],
+					['lettertile', 'lettertile'],
+					['wordsnack', 'wordsnack'],
+					['doodlecrew', 'doodlecrew'],
+					['awkword', 'awkword'],
+					['spellcast', 'spellcast'],
+					['checkers', 'checkers'],
+					['puttparty', 'puttparty'],
+				])
+		),
 	execute: async (interaction: any) => {
-		if(interaction.channel.type === 'DM') {
-			interaction.reply('Do you even have friends lol, doing it in DM...\nIt only works in server')
+		if (interaction.channel.type === 'DM') {
+			interaction.reply(
+				'Do you even have friends lol, doing it in DM...\nIt only works in server'
+			)
 			return
 		}
 		await interaction.deferReply()
-		const activity_type = interaction.options.get('activity_type')?.value as string
-		if(typeof activity.application_id[activity_type] === 'undefined') {
+		const activity_type = interaction.options.get('activity_type')
+			?.value as string
+		if (typeof activity.application_id[activity_type] === 'undefined') {
 			interaction.editReply('Invalid activity')
 			return
 		}
-		if(!interaction.member.voice.channel) {
+		if (!interaction.member.voice.channel) {
 			interaction.editReply('Please join a voice channel first')
 			return
 		}
-		const fetch_obj = await fetch(`https://discord.com/api/v8/channels/${interaction.member.voice.channel.id}/invites`, {
-			method: 'POST',
-			body: JSON.stringify({
-				max_age: 86400,
-				max_uses: 0,
-				target_application_id: activity.application_id[activity_type],
-				target_type: 2,
-				temporary: false,
-				validate: null
-			}),
-			headers: {
-				'Authorization': `Bot ${process.env.TOKEN}`,
-				'Content-Type': 'application/json'
+		const fetch_obj = await fetch(
+			`https://discord.com/api/v8/channels/${interaction.member.voice.channel.id}/invites`,
+			{
+				method: 'POST',
+				body: JSON.stringify({
+					max_age: 86400,
+					max_uses: 0,
+					target_application_id:
+                        activity.application_id[activity_type],
+					target_type: 2,
+					temporary: false,
+					validate: null,
+				}),
+				headers: {
+					Authorization: `Bot ${process.env.TOKEN}`,
+					'Content-Type': 'application/json',
+				},
 			}
-		})
+		)
 		const res = await fetch_obj.json()
 		const invite_link = `https://discord.gg/${res.code}`
-		interaction.editReply(`${activity.application_name[activity_type]}: ${invite_link}`)
+		interaction.editReply(
+			`${activity.application_name[activity_type]}: ${invite_link}`
+		)
 	},
 	application_id: {
-		'youtube_together': '755600276941176913',
-		'watch_together_dev': '880218832743055411',
-		'fishington': '814288819477020702',
-		'chess_in_the_park': '832012774040141894',
-		'chess_in_the_park_dev': '832012586023256104',
-		'betrayal': '773336526917861400',
-		'doodlecrew': '878067389634314250',
-		'wordsnacks': '879863976006127627',
-		'lettertile': '879863686565621790',
-		'poker_night': '755827207812677713'
+		youtube: '880218394199220334',
+		youtubedev: '880218832743055411',
+		poker: '755827207812677713',
+		betrayal: '773336526917861400',
+		fishing: '814288819477020702',
+		chess: '832012774040141894',
+		chessdev: '832012586023256104',
+		lettertile: '879863686565621790',
+		wordsnack: '879863976006127627',
+		doodlecrew: '878067389634314250',
+		awkword: '879863881349087252',
+		spellcast: '852509694341283871',
+		checkers: '832013003968348200',
+		puttparty: '763133495793942528',
 	},
 	application_name: {
-		'youtube_together': 'Youtube Together',
-		'watch_together_dev': 'Watch Together Dev',
-		'fishington': 'Fishington.io',
-		'chess_in_the_park': 'Chess in the Park',
-		'chess_in_the_park_dev': 'Chess in the Park Development',
-		'betrayal': 'Betrayal.io',
-		'doodlecrew': 'Doodle Crew',
-		'wordsnacks': 'Word Snacks',
-		'lettertile': 'Letter Tile',
-		'poker_night': 'Poker Night'
-	}
+		youtube: 'Youtube Together',
+		youtubedev: 'Youtube Together Dev',
+		poker: 'Poker Night',
+		betrayal: 'Betrayal',
+		fishing: 'Fishington', 
+		chess: 'Chess in Park',
+		chessdev: 'Chess in Park Dev',
+		lettertile: 'Letter Tile',
+		wordsnack: 'Word Snack',
+		doodlecrew: 'Doodle Crew',
+		awkword: 'Awkword',
+		spellcast: 'SpellCast',
+		checkers: 'Checkers',
+		puttparty: 'Puttparty',
+	},
 }
 
 export default activity
